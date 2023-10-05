ad4bae4d24444197c61c3c79448546459e89b67a
AnandhSrinivasan <devanandh2902@gmail.com>
2023-10-05 10:09:38 +0530

    Initial commit

---
+++ Class_Notes/src/examples/Q9_Copy_Array.java
@@ -0,0 +1,23 @@
+package examples;
+
+import java.util.ArrayList;
+
+public class Q9_Copy_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> arrayList = new ArrayList<String>();
+		arrayList.add("Kailash");
+		arrayList.add("Deva");
+		arrayList.add("Anandh");
+		arrayList.add("Madhavan");
+		arrayList.add("Kalivardhan");
+		arrayList.add("Ravanan");
+		System.out.println(arrayList);
+		ArrayList<String> arrayList1 = new ArrayList<String>();
+		arrayList1.addAll(arrayList);
+		System.out.println(arrayList1);
+	}
+
+}
